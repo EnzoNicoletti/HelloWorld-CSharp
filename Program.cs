dfd172efa3f63273c6c6c5cbfd2924a641adff23
EnzoNicoletti <enzo.nicoletti@aluno.senai.br>
2025-07-31 10:33:31 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var num = 0;
+        var num2 = 1;
+        var soma = num + num2;
+        Console.WriteLine("A soma da: " + soma);
+        Console.WriteLine("fort");
+        Console.WriteLine("night");
+    }
+}
